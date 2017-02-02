57d99b71f727956e15ce6f5e0c78c2ff88fe00b0
kassadube <noamsrb@yahoo.com>
2017-02-02 18:08:13 +0200

    fix structure

--- src/app/index.js
+++ src/app/index.js
@@ -5,70 +5,12 @@ import logger from "redux-logger";
 import {Provider} from "react-redux";
 
 import App from './container/App';
+import store from "./store";
 
 
 
-const initialMathState = {
-    result: 1,
-    lastValues: []
-}
-
-const mathReducer = (state = initialMathState, action) => {
-    switch (action.type) {
-        case "ADD":
-            state = {
-                ...state,
-                result: state.result + action.payload,
-                lastValues: [...state.lastValues, action.payload]
-            };
-            break;
-        case "SUBTRACT":
-            state = {
-                ...state,
-                result: state.result - action.payload,
-                lastValues: [...state.lastValues, action.payload]
-            };
-            break;
-        default:
-            break;
-    }
-    return state;
-}
-
-const initialUserState = {
-    name: "moshe",
-    age: 99
-}
-
-const userReducer = (state = initialUserState, action) => {
-    switch (action.type) {
-        case "SET_NAME":
-            state = {
-                ...state,
-                name: action.payload
-            };
-            break;
-        case "SET_AGE":
-            state = {
-                ...state,
-                age: action.payload
-            };
-            break;
-        default:
-            break;
-    }
-    return state;
-}
 
 
-const myLogger = (store) => (next) => (action) => {
-    console.log("Logged Action: ", action);
-    next(action);
-}
-
-const store = createStore(combineReducers({ mathReducer, userReducer }),
-    {},
-    applyMiddleware(myLogger, logger()));
 
 store.subscribe(() => {
     // console.log("store updated", store.getState());
---
+++ src/app/store.js
@@ -0,0 +1,19 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import mathReducer  from "./reducers/mathReducer";
+import userReducer  from "./reducers/userReducer";
+
+
+
+const myLogger = (store) => (next) => (action) => {
+    console.log("Logged Action: ", action);
+    next(action);
+}
+
+
+
+export default createStore(combineReducers(
+    { mathReducer, userReducer }),
+    {},
+    applyMiddleware(myLogger, logger())
+    );
